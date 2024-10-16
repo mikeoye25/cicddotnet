95c8cd77f9765667ed15efe38ee9592be0f4aaac
Michael Oyelami <michael.oyelami@frontdoor.com>
2024-10-16 15:00:42 +0100

    updates dotnet.yml fil again e

--- cicddotnet/Program.cs
+++ cicddotnet/Program.cs
@@ -1,6 +1,6 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! This is an update");
+app.MapGet("/", () => "Hello World! Another update test");
 
 app.Run();
